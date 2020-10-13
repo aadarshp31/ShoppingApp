70a34ac6c957f84375df2cc69327fb43892dcb92
aadarshp31 <adarshpandey.in@gmail.com>
2020-10-13 20:04:32 +0530

    Created Product component and HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
 import { Container } from 'react-bootstrap'
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h2>Welcome to Shopping Website</h2>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
