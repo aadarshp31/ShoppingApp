ce3a852eda5c4ce7171e03f664a2996f15a9aa9e
aadarshp31 <adarshpandey.in@gmail.com>
2020-10-13 18:52:41 +0530

    Created Header, Footer component with navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
 
 const App = () => {
   return (
-    <div className="App">
-      <h2>Welcome to Shopping Website</h2>
-    </div>
-  );
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h2>Welcome to Shopping Website</h2>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
