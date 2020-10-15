528c56855222d58bcafd173d1aa7af231ced6510
aadarshp31 <adarshpandey.in@gmail.com>
2020-10-15 18:30:45 +0530

    Connected frontend with backend using axios
    Also removed the template data products.js from frontend.
    Added 'proxy' key to frontend package.json to make HTTP calls to another port on the same localhost.
    Used component level states in HomeScreen and ProductScreen to store and display data from the backend.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,19 @@
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
 import Product from '../components/Product'
 import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
+
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  const fetchProducts = async () => {
+    const { data } = await axios.get('/api/products')
+    setProducts(data)
+  }
   return (
     <>
       <h1>Latest Products</h1>
