0019dd79914f86952bf6c31dca8e5b1e786511e1
aadarshp31 <adarshpandey.in@gmail.com>
2020-10-17 15:06:27 +0530

    Created errorhandlers using express middlewares
    Also modified error response to provide similarly formatted json error response to the client.
    Overridden the default errorhandler to make sure only json response is sent to the client and not html response for the express server.

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,22 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 
-const app = express()
 dotenv.config()
 
 connectDB()
+
+const app = express()
 
 app.get('/', (req, res) => {
   res.send('API is running!')
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
