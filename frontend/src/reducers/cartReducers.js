c917d25ed04e05c5f038cbadabda1728c3040e54
aadarshp31 <adarshpandey.in@gmail.com>
2020-10-19 17:25:22 +0530

    Added Redux State for cart in frontend
    Added Constants, Reducer, Action to manage our state using redux.
    Also added functionality to add and get data to and from the localstorage of the browser

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,29 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants.js'
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const itemExists = state.cartItems.find(
+        item => item.product === action.payload.product
+      )
+
+      if (!!itemExists) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map(item =>
+            item.product === itemExists.product ? action.payload : item
+          ),
+        }
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, action.payload],
+        }
+      }
+    case CART_REMOVE_ITEM:
+      return {}
+
+    default:
+      return state
+  }
+}
