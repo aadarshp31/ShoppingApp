5a99be59548124f4d15ef070d34700fdbf569ac1
aadarshp31 <adarshpandey.in@gmail.com>
2020-10-20 20:28:05 +0530

    Added routes and controller for the userLogin
    Also added express.json() middleware to parse the incoming req.body as JSON data

---
+++ backend/controllers/userController.js
@@ -0,0 +1,29 @@
+import User from '../models/userModel.js'
+import asyncHandler from 'express-async-handler'
+
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  let { email, password } = req.body
+  email = email.toLowerCase()
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    res.status(404)
+    throw new Error(`User with email ${email} does not exists!`)
+  } else if (await user.matchPassword(password)) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid Password')
+  }
+})
+
+export { authUser }
