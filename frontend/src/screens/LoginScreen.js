97b9ede063927efa7286ac1283024ce52afe5708
aadarshp31 <adarshpandey.in@gmail.com>
2020-11-01 15:16:38 +0530

    Added the 'submitHandler' method with 'dispatch' from Redux.
    Added Error message and Loader component to handler error and loading states.
    Added redirect using history api to redirect the loggedIn user to the homepage.

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -7,21 +7,34 @@ import Loader from '../components/Loader.js'
 import FormContainer from '../components/FormContainer.js'
 import { login } from '../actions/userActions.js'
 
-const LoginScreen = ({ location }) => {
+const LoginScreen = ({ location, history }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const userLogin = useSelector((state) => state.userLogin)
+  const { loading, error, userInfo } = userLogin
+
+  const dispatch = useDispatch()
+
   const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect)
+    }
+  }, [history, userInfo, redirect])
 
   const submitHandler = (e) => {
     e.preventDefault()
-    // DISPATCH Login
+    dispatch(login(email, password))
   }
 
   return (
     <FormContainer>
       <h1>Sign In</h1>
-      <Form action={submitHandler}>
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
         <Form.Group controlId='email'>
           <Form.Label>Email Address</Form.Label>
           <Form.Control
