58684f5367547a6fd3b63ca118802139e77dc21b
aadarshp31 <adarshpandey.in@gmail.com>
2020-11-06 23:57:56 +0530

    Added RegisteScreen compoment to handle user registration
    Also added a password validation check

---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message.js'
+import Loader from '../components/Loader.js'
+import FormContainer from '../components/FormContainer.js'
+import { register } from '../actions/userActions.js'
+
+const RegisterScreen = ({ location, history }) => {
+  const [name, setName] = useState('')
+  const [lastname, setLastname] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [validationMessage, setValidationMessage] = useState(null)
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { loading, error, userInfo } = userLogin
+
+  const dispatch = useDispatch()
+
+  const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect)
+    }
+  }, [history, userInfo, redirect])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (password !== confirmPassword) {
+      setValidationMessage("Passwords don't match! try again...")
+      document.getElementById('password').focus()
+    }
+    dispatch(register(email, password, name, lastname))
+  }
+
+  return (
+    <FormContainer>
+      <h1>Sign Up</h1>
+      {validationMessage && (
+        <Message variant='danger'>{validationMessage}</Message>
+      )}
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='name'>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='lastname'>
+          <Form.Label>Lastname</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter lastname'
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+          <Form.Text className='text-muted'>
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId='password'>
+          <Form.Label>Enter Password</Form.Label>
+          <Form.Control
+            id='password'
+            type='password'
+            placeholder='Enter Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+          <Form.Text className='text-muted'>
+            We'll never share your password with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId='confirmPassword'>
+          <Form.Label>Confirm password</Form.Label>
+          <Form.Control
+            id='confirmPassword'
+            type='password'
+            placeholder='Confirm password'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+          <Form.Text className='text-muted'>
+            We'll never share your password with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Button type='submit' variant='primary'>
+          Sign Up
+        </Button>
+      </Form>
+
+      <Row className='py-3'>
+        <Col>
+          New Customer?{' '}
+          <Link to={redirect ? `/login/redirect=${redirect}` : `/login`}>
+            Login
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  )
+}
+
+export default RegisterScreen
